f8cd750a3201389dbd3e50e797238b7109f0082f
Nynra <nynradev@pm.me>
2025-03-24 15:02:49 +0100

    Added some tapers

---
+++ src/pytools_litho_design/components/tapers.py
@@ -0,0 +1,162 @@
+"""Geometries contains useful shapes/tools that are not available in phidl's
+geometry library."""
+
+import numpy as np
+from typing import Tuple, List, Union, Optional
+import gdsfactory as gf
+
+
+@gf.cell
+def hyper_taper(length=10, wide_section=50, narrow_section=5, layer=1) -> gf.Component:
+    """Hyperbolic taper (solid). Designed by colang.
+
+    See the original code at [qnngds](https://github.com/qnngroup/qnngds/tree/master?tab=readme-ov-file).
+    The code was modified to use gdsfactory.
+
+    Parameters
+    ----------
+    length: int or float
+        Length of taper.
+    wide_section: int or float
+        Wide width dimension.
+    narrow_section: int or float
+        Narrow width dimension.
+    layer: int
+        Layer for device to be created on.
+
+    Returns
+    -------
+    Component
+        The hyper taper.
+    """
+
+    taper_length = length
+    wide = wide_section
+    zero = 0
+    narrow = narrow_section
+    x_list = np.arange(0, taper_length + 0.1, 0.1)
+    x_list2 = np.arange(taper_length, -0.1, -0.1)
+    pts = []
+
+    a = np.arccosh(wide / narrow) / taper_length
+
+    for x in x_list:
+        pts.append((x, np.cosh(a * x) * narrow / 2))
+    for y in x_list2:
+        pts.append((y, -np.cosh(a * y) * narrow / 2))
+        HT = gf.Component()
+        _ = HT.add_polygon(pts, layer=layer)
+        HT.add_port(name="1", center=[0, 0], width=narrow, orientation=180, layer=layer)
+        HT.add_port(
+            name="2", center=[taper_length, 0], width=wide, orientation=0, layer=layer
+        )
+        HT.flatten()
+    return HT
+
+
+@gf.cell
+def angled_taper(
+    wire_width: Union[int, float] = 0.2,
+    constr_width: Union[int, float] = 0.1,
+    angle: Union[int, float] = 60,
+    layer: int = 1,
+) -> gf.Component:
+    """Create an angled taper with euler curves
+
+    See the original code at [qnngds](https://github.com/qnngroup/qnngds/tree/master?tab=readme-ov-file)
+    The code was modified to use gdsfactory.
+
+    Parameters
+    ----------
+    wire_width: int or float
+        Width of wide end of taper
+    constr_width: int or float
+        Width of narrow end of taper
+    angle: int or float
+        Angle between taper ends in degrees
+
+    Returns
+    -------
+    Components
+        A gdsfactory Component containing a single taper
+    """
+
+    D = gf.Component()
+
+    # heuristic for length between narrow end and bend
+    l_constr = constr_width * 2 + wire_width * 2
+    # heuristic for length between wide end and bend
+    l_wire = constr_width * 2 + wire_width * 2
+    sin = np.sin(angle * np.pi / 180)
+    cos = np.cos(angle * np.pi / 180)
+    # path along the center of the taper
+    p_center = np.array(
+        [[0, 0], [l_constr, 0], [l_constr + l_wire * cos, l_wire * sin]]
+    )
+    # upper (shorter) path along the inside edge of the taper
+    p_upper = np.array(
+        [
+            [0, constr_width / 2],
+            [0, constr_width / 2],
+            p_center[2] + [-wire_width / 2 * sin, wire_width / 2 * cos],
+        ]
+    )
+    p_upper[1, 0] = (constr_width / 2 - p_upper[2, 1]) * cos / sin + p_upper[2, 0]
+    # lower (longer) path along the outside edge of the taper
+    p_lower = np.array(
+        [
+            [0, -constr_width / 2],
+            [0, -constr_width / 2],
+            p_center[2] + [wire_width / 2 * sin, -wire_width / 2 * cos],
+        ]
+    )
+    p_lower[1, 0] = (-constr_width / 2 - p_lower[2, 1]) * cos / sin + p_lower[2, 0]
+
+    # interpolate euler curve between points using numpy
+    # P_upper = pp.smooth(
+    #     points=p_upper, radius=wire_width, corner_fun=pp.euler, use_eff=False
+    # )
+    # P_lower = pp.smooth(
+    #     points=p_lower, radius=wire_width, corner_fun=pp.euler, use_eff=False
+    # )
+    P_upper = gf.path.smooth(
+        points=p_upper, radius=wire_width, bend=gf.path.euler, use_eff=False
+    )
+    P_lower = gf.path.smooth(
+        points=p_lower, radius=wire_width, bend=gf.path.euler, use_eff=False
+    )
+
+    # create a polygon
+    points = np.concatenate((P_upper.points, P_lower.points[::-1]))
+    D.add_polygon(points, layer=layer)
+
+    # port 1: narrow/constr_width end, port 2: wide/wire_width end
+    D.add_port(
+        name="1",
+        center=(P_upper.points[0] + P_lower.points[0]) / 2,
+        width=constr_width,
+        orientation=180,
+        layer=layer,
+    )
+    D.add_port(
+        name="2",
+        center=(P_upper.points[-1] + P_lower.points[-1]) / 2,
+        width=wire_width,
+        orientation=angle,
+        layer=layer,
+    )
+    return D
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    # Create and show the hyperbolic taper
+    c = hyper_taper()
+    c.plot()
+    plt.show()
+
+    # Create and show the angled taper
+    c = angled_taper()
+    c.plot()
+    plt.show()
