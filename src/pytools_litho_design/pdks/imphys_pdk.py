0a52bb18e717083b7b7f24818023a1f723a438f2
Nynra <nynradev@pm.me>
2025-04-14 12:04:07 +0200

    Updated cross sections and imphys pdk

--- src/pytools_litho_design/pdks/imphys_pdk.py
+++ src/pytools_litho_design/pdks/imphys_pdk.py
@@ -39,7 +39,10 @@ class IMPHYS_LAYERS(LayerMap):
     AU_PADS: Layer = (1, 0)  # Gold pads for wire bonding
     MARKERS: Layer = (2, 0)  # Markers for alignment and registration
     NEG_NBTIN: Layer = (3, 0)  # Negative resist NBTIN for superconducting lines
-    SIO2: Layer = (4, 0)  # Silicon dioxide for insulation
+    NBTIN: Layer = (4, 0)  # NBTIN layer for superconducting lines
+    SIO2: Layer = (5, 0)  # Silicon dioxide for insulation
+    SHALLOW_ETCH: Layer = (6, 0)  # Shallow etch for patterning
+    DEEP_ETCH: Layer = (7, 0)  # Deep etch for patterning
     WAVEGUIDE: Layer = (8, 0)  # Waveguide layer for photonic circuits
     CLADDING: Layer = (9, 0)  # Cladding layer for waveguides
 
@@ -55,6 +58,12 @@ IMPHYS_LAYER_TRANSITIONS = {
     IMPHYS_LAYERS.NEG_NBTIN: partial(
         gf.components.taper_electrical,
         layer=IMPHYS_LAYERS.NEG_NBTIN,
+        port_names=("e1", "e2"),
+        port_types=("electrical", "electrical"),
+    ),
+    IMPHYS_LAYERS.NBTIN: partial(
+        gf.components.taper_electrical,
+        layer=IMPHYS_LAYERS.NBTIN,
         port_names=("e1", "e2"),
         port_types=("electrical", "electrical"),
     ),
